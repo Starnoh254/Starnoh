3cac02ecf5fbe1850a4b67e1e28dd1b015639517
Starnoh John <130779431+Starnoh254@users.noreply.github.com>
2025-05-01 13:51:36 +0300

    Replace Navbar component with NavBarTemp and remove old navbar implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/NavBar";
+import Navbar from "./components/NavBarTemp";
 import Home from "./pages/home";
 import Contact from "./pages/Contact";
 import Projects from "./pages/Projects";
